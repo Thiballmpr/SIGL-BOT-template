9380e3e0cb5cf9164e02c78384b9b2c9ca5f29be
Thiballmpr <thibaultlempereur@gmail.com>
2021-09-29 21:16:33 +0200

    push main

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
 import os
+from discord import guild, message, user
+import discord
+from discord.abc import User
 from dotenv import load_dotenv
 from discord.ext import commands
 
@@ -19,6 +22,20 @@ async def on_ready():  # When the bot is ready
 @bot.command()
 async def pong(ctx):
     await ctx.send('pong')
+
+@bot.command()
+async def name(ctx):
+    await ctx.send(ctx.author)
+
+@bot.command()
+async def admin(ctx, arg):
+    roles = ctx.guild
+    if not roles.has_role(name="Admin"):
+        ctx.guild.create_roles(name='Admin', colour=discord.Colour(0x0062ff))
+        perms = discord.Permissions(manage_channels=True, kick_members=True, ban_members=True)
+        ctx.guild.edit
+    ctx.add_roles(arg, 'Admin')
+
 
 token = os.getenv("DISCORD_TOKEN")
 bot.run(token)  # Starts the bot
